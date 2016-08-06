8eeff4b096350e2731d1a1aeb3879d42b3931986
ArturGodunov <artur-godunov@mail.ru>
2016-08-06 09:40:10 +0300

    Lesson 8: Task 1

---
+++ 008_AJAX/Samples/Task/Task1/todo.js
@@ -0,0 +1,32 @@
+﻿var todoModel = (function () {
+
+    var _data = [];
+
+    function _addItem(name, duedate, description, completed) {
+        _data.push({
+            name: name,
+            duedate: duedate,
+            description: description,
+            completed: completed
+        });
+    }
+
+    function _read() {
+        var temp = window.localStorage["tasks"];
+
+        if (!temp) _data = [];
+        else _data = JSON.parse(temp);
+
+        return _data;
+    }
+
+    return {
+        data: _data,
+        addItem: _addItem,
+        updateItem: _updateItem,
+        removeItem: _removeItem,
+        save: _save,
+        read: _read
+    };
+
+})();
